878fb77582ac30b9623538219dd9c399bea5b112
alex05570 <145584857+alex05570@users.noreply.github.com>
2026-09-24 12:48:59 +0300

    Test PR

--- src/main/java/org/example/taskflow/task/TaskNotFoundException.java
+++ src/main/java/org/example/taskflow/task/TaskNotFoundException.java
@@ -2,6 +2,6 @@ package org.example.taskflow.task;
 
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(Long id) {
-        super("Task not found: " + id);
+        super("Task not found:  " + id);
     }
 }
